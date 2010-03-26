9d5588e8e243da9240a8bcfee24432102ee8a3e8
PriMoThemes <PriMoThemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-26 19:35:08 +0000

    Version 2.7 released with new features and compatibility for BuddyPress.
    git-svn-id: https://plugins.svn.wordpress.org/s2member/trunk@221901 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/functions/activate-deactivate.inc.php
@@ -0,0 +1,182 @@
+<?php
+/*
+Copyright: © 2009 WebSharks, Inc. ( coded in the USA )
+<mailto:support@websharks-inc.com> <http://www.websharks-inc.com/>
+
+Released under the terms of the GNU General Public License.
+You should have received a copy of the GNU General Public License,
+along with this software. In the main directory, see: /licensing/
+If not, see: <http://www.gnu.org/licenses/>.
+*/
+/*
+Direct access denial.
+*/
+if (realpath (__FILE__) === realpath ($_SERVER["SCRIPT_FILENAME"]))
+	exit;
+/*
+Function for handling activation routines.
+This function should match the array key for this plugin:
+ws_plugin__$plugin_key_activate() is called by our themes.
+
+We also initialize some option values here.
+Initializing these options will force them to be
+autoloaded into WordPress® instead of generating
+extra queries before they are set.
+*/
+function ws_plugin__s2member_activate ()
+	{
+		add_role ("s2member_level1", "s2Member Level 1");
+		add_role ("s2member_level2", "s2Member Level 2");
+		add_role ("s2member_level3", "s2Member Level 3");
+		add_role ("s2member_level4", "s2Member Level 4");
+		/**/
+		$role = &get_role ("s2member_level1");
+		$role->add_cap ("read");
+		$role->add_cap ("level_0");
+		$role->add_cap ("access_s2member_level1");
+		/**/
+		$role = &get_role ("s2member_level2");
+		$role->add_cap ("read");
+		$role->add_cap ("level_0");
+		$role->add_cap ("access_s2member_level2");
+		$role->add_cap ("access_s2member_level1");
+		/**/
+		$role = &get_role ("s2member_level3");
+		$role->add_cap ("read");
+		$role->add_cap ("level_0");
+		$role->add_cap ("access_s2member_level3");
+		$role->add_cap ("access_s2member_level2");
+		$role->add_cap ("access_s2member_level1");
+		/**/
+		$role = &get_role ("s2member_level4");
+		$role->add_cap ("read");
+		$role->add_cap ("level_0");
+		$role->add_cap ("access_s2member_level4");
+		$role->add_cap ("access_s2member_level3");
+		$role->add_cap ("access_s2member_level2");
+		$role->add_cap ("access_s2member_level1");
+		/**/
+		$role = &get_role ("administrator");
+		$role->add_cap ("access_s2member_level1");
+		$role->add_cap ("access_s2member_level2");
+		$role->add_cap ("access_s2member_level3");
+		$role->add_cap ("access_s2member_level4");
+		/**/
+		$role = &get_role ("editor");
+		$role->add_cap ("access_s2member_level1");
+		$role->add_cap ("access_s2member_level2");
+		$role->add_cap ("access_s2member_level3");
+		$role->add_cap ("access_s2member_level4");
+		/**/
+		$role = &get_role ("author");
+		$role->add_cap ("access_s2member_level1");
+		$role->add_cap ("access_s2member_level2");
+		$role->add_cap ("access_s2member_level3");
+		$role->add_cap ("access_s2member_level4");
+		/**/
+		$role = &get_role ("contributor");
+		$role->add_cap ("access_s2member_level1");
+		$role->add_cap ("access_s2member_level2");
+		$role->add_cap ("access_s2member_level3");
+		$role->add_cap ("access_s2member_level4");
+		/**/
+		if (!is_dir ($files_dir = $GLOBALS["WS_PLUGIN__"]["s2member"]["c"]["files_dir"]))
+			if (is_writable (dirname ($files_dir)))
+				mkdir ($files_dir, 0777);
+		/**/
+		if (is_dir ($files_dir) && is_writable ($files_dir))
+			if (!file_exists ($htaccess = $files_dir . "/.htaccess"))
+				file_put_contents ($htaccess, "deny from all");
+		/**/
+		if (!is_dir ($logs_dir = $GLOBALS["WS_PLUGIN__"]["s2member"]["c"]["logs_dir"]))
+			if (is_writable (dirname ($logs_dir)))
+				mkdir ($logs_dir, 0777);
+		/**/
+		if (is_dir ($logs_dir) && is_writable ($logs_dir))
+			if (!file_exists ($htaccess = $logs_dir . "/.htaccess"))
+				file_put_contents ($htaccess, "deny from all");
+		/**/
+		if (!is_numeric (get_option ("ws_plugin__s2member_configured")))
+			update_option ("ws_plugin__s2member_configured", "0");
+		/**/
+		if (!is_array (get_option ("ws_plugin__s2member_cache")))
+			update_option ("ws_plugin__s2member_cache", array ());
+		/**/
+		if (!is_array (get_option ("ws_plugin__s2member_notices")))
+			update_option ("ws_plugin__s2member_notices", array ());
+		/**/
+		if (!is_array (get_option ("ws_plugin__s2member_options")))
+			update_option ("ws_plugin__s2member_options", array ());
+		/**/
+		return;
+	}
+/*
+Function for handling de-activation cleanup routines.
+This function should match the array key for this plugin:
+ws_plugin__$plugin_key_deactivate() is called by our themes.
+*/
+function ws_plugin__s2member_deactivate ()
+	{
+		remove_role ("s2member_level1");
+		remove_role ("s2member_level2");
+		remove_role ("s2member_level3");
+		remove_role ("s2member_level4");
+		/**/
+		$role = &get_role ("administrator");
+		$role->remove_cap ("access_s2member_level1");
+		$role->remove_cap ("access_s2member_level2");
+		$role->remove_cap ("access_s2member_level3");
+		$role->remove_cap ("access_s2member_level4");
+		/**/
+		$role = &get_role ("editor");
+		$role->remove_cap ("access_s2member_level1");
+		$role->remove_cap ("access_s2member_level2");
+		$role->remove_cap ("access_s2member_level3");
+		$role->remove_cap ("access_s2member_level4");
+		/**/
+		$role = &get_role ("author");
+		$role->remove_cap ("access_s2member_level1");
+		$role->remove_cap ("access_s2member_level2");
+		$role->remove_cap ("access_s2member_level3");
+		$role->remove_cap ("access_s2member_level4");
+		/**/
+		$role = &get_role ("contributor");
+		$role->remove_cap ("access_s2member_level1");
+		$role->remove_cap ("access_s2member_level2");
+		$role->remove_cap ("access_s2member_level3");
+		$role->remove_cap ("access_s2member_level4");
+		/**/
+		if (is_dir ($files_dir = $GLOBALS["WS_PLUGIN__"]["s2member"]["c"]["files_dir"]))
+			{
+				if (file_exists ($htaccess = $files_dir . "/.htaccess"))
+					if (is_writable ($htaccess))
+						unlink ($htaccess);
+				/**/
+				@rmdir ($files_dir);
+			}
+		/**/
+		if (is_dir ($logs_dir = $GLOBALS["WS_PLUGIN__"]["s2member"]["c"]["logs_dir"]))
+			{
+				if (file_exists ($htaccess = $logs_dir . "/.htaccess"))
+					if (is_writable ($htaccess))
+						unlink ($htaccess);
+				/**/
+				if (file_exists ($log = $logs_dir . "/paypal-ipn.log"))
+					if (is_writable ($log))
+						unlink ($log);
+				/**/
+				if (file_exists ($log = $logs_dir . "/paypal-rtn.log"))
+					if (is_writable ($log))
+						unlink ($log);
+				/**/
+				@rmdir ($logs_dir);
+			}
+		/**/
+		delete_option ("ws_plugin__s2member_configured");
+		delete_option ("ws_plugin__s2member_cache");
+		delete_option ("ws_plugin__s2member_notices");
+		delete_option ("ws_plugin__s2member_options");
+		/**/
+		return;
+	}
+?>
---
+++ includes/functions/register-access.inc.php
@@ -0,0 +1,122 @@
+<?php
+/*
+Copyright: © 2009 WebSharks, Inc. ( coded in the USA )
+<mailto:support@websharks-inc.com> <http://www.websharks-inc.com/>
+
+Released under the terms of the GNU General Public License.
+You should have received a copy of the GNU General Public License,
+along with this software. In the main directory, see: /licensing/
+If not, see: <http://www.gnu.org/licenses/>.
+*/
+/*
+Direct access denial.
+*/
+if (realpath (__FILE__) === realpath ($_SERVER["SCRIPT_FILENAME"]))
+	exit;
+/*
+Function for checking registration form access.
+Attach to: add_filter("pre_option_users_can_register");
+*/
+function ws_plugin__s2member_check_register_access ($users_can_register = FALSE)
+	{
+		if ($users_can_register)
+			{
+				return ($users_can_register = true);
+			}
+		else if ($_COOKIE["s2member_subscr_id"] && $_COOKIE["s2member_custom"] && preg_match ("/^[1-4]$/", $_COOKIE["s2member_level"]))
+			{
+				global $wpdb; /* Global database object reference. */
+				/**/
+				if (!$usermeta = $wpdb->get_row ("SELECT `user_id` FROM `" . $wpdb->usermeta . "` WHERE `meta_key` = 's2member_subscr_id' AND `meta_value` = '" . $wpdb->escape ($_COOKIE["s2member_subscr_id"]) . "' LIMIT 1"))
+					{
+						return ($users_can_register = true);
+					}
+			}
+		/**/
+		return $users_can_register;
+	}
+/*
+This adds custom fields to the registration form.
+Attach to: add_action("register_form");
+*/
+function ws_plugin__s2member_custom_registration_fields ()
+	{
+		if ($_COOKIE["s2member_subscr_id"] && $_COOKIE["s2member_custom"] && preg_match ("/^[1-4]$/", $_COOKIE["s2member_level"]) && ($tabindex = 20))
+			{
+				echo '<p>' . "\n";
+				echo '<label>' . "\n";
+				echo 'First Name' . "\n";
+				echo '<input type="text" name="s2member_custom_reg_field_first_name" id="s2member-custom-reg-field-first-name" class="s2member-custom-reg-field input" size="25" tabindex="' . esc_attr (($tabindex = $tabindex + 1)) . '" value="' . format_to_edit (trim (stripslashes ($_REQUEST["s2member_custom_reg_field_first_name"]))) . '" maxlength="100" />' . "\n";
+				echo '</label>' . "\n";
+				echo '</p>';
+				/**/
+				echo '<p>' . "\n";
+				echo '<label>' . "\n";
+				echo 'Last Name' . "\n";
+				echo '<input type="text" name="s2member_custom_reg_field_last_name" id="s2member-custom-reg-field-last-name" class="s2member-custom-reg-field input" size="25" tabindex="' . esc_attr (($tabindex = $tabindex + 1)) . '" value="' . format_to_edit (trim (stripslashes ($_REQUEST["s2member_custom_reg_field_last_name"]))) . '" maxlength="100" />' . "\n";
+				echo '</label>' . "\n";
+				echo '</p>';
+				/**/
+				foreach (preg_split ("/[\r\n\t,;]+/", $GLOBALS["WS_PLUGIN__"]["s2member"]["o"]["custom_reg_fields"]) as $field)
+					{
+						echo '<p>' . "\n";
+						echo '<label>' . "\n";
+						echo esc_html (trim ($field)) . "\n";
+						$field = preg_replace ("/[^a-z0-9]/i", "_", strtolower (trim ($field)));
+						echo '<input type="text" name="s2member_custom_reg_field_' . esc_attr ($field) . '" id="s2member-custom-reg-field-' . esc_attr (preg_replace ("/_/", "-", $field)) . '" class="s2member-custom-reg-field input" size="25" tabindex="' . esc_attr (($tabindex = $tabindex + 1)) . '" value="' . format_to_edit (trim (stripslashes ($_REQUEST["s2member_custom_reg_field_" . $field]))) . '" maxlength="100" />' . "\n";
+						echo '</label>' . "\n";
+						echo '</p>';
+					}
+			}
+		/**/
+		return;
+	}
+/*
+Function for configuring the registration of new users.
+Attach to: add_action("user_register");
+Attach to: add_action("bp_core_signup_user");
+*/
+function ws_plugin__s2member_configure_user_registration ($user_id = FALSE)
+	{
+		static $processed = false; /* Prevents duplicate processing when attached to multiple hooks in support of plugins like BuddyPress. */
+		/**/
+		if (!$processed && $user_id && $_COOKIE["s2member_subscr_id"] && $_COOKIE["s2member_custom"] && preg_match ("/^[1-4]$/", $_COOKIE["s2member_level"]) && ($processed = true))
+			{
+				global $wpdb; /* We need to check the uniqueness of their subscription id. */
+				/**/
+				if (!$usermeta = $wpdb->get_row ("SELECT `user_id` FROM `" . $wpdb->usermeta . "` WHERE `meta_key` = 's2member_subscr_id' AND `meta_value` = '" . $wpdb->escape ($_COOKIE["s2member_subscr_id"]) . "' LIMIT 1"))
+					{
+						$_POST = stripslashes_deep ($_POST);
+						/**/
+						ws_plugin__s2member_email_config ();
+						/**/
+						$user = new WP_User ($user_id);
+						$user->set_role ("s2member_level" . $_COOKIE["s2member_level"]);
+						/**/
+						update_usermeta ($user_id, "s2member_subscr_id", $_COOKIE["s2member_subscr_id"]);
+						update_usermeta ($user_id, "s2member_custom", $_COOKIE["s2member_custom"]);
+						/**/
+						if (!defined ("BP_VERSION")) /* Custom fields are not compatible when running together with BuddyPress. */
+							{
+								update_usermeta ($user_id, "first_name", ($first_name = trim ($_POST["s2member_custom_reg_field_first_name"])));
+								update_usermeta ($user_id, "last_name", ($last_name = trim ($_POST["s2member_custom_reg_field_last_name"])));
+								wp_update_user (array ("ID" => $user_id, "display_name" => $first_name));
+								/**/
+								foreach (preg_split ("/[\r\n\t,;]+/", $GLOBALS["WS_PLUGIN__"]["s2member"]["o"]["custom_reg_fields"]) as $field)
+									{
+										$field = preg_replace ("/[^a-z0-9]/i", "_", strtolower (trim ($field)));
+										$fields[$field] = trim ($_POST["s2member_custom_reg_field_" . $field]);
+									}
+								/**/
+								update_usermeta ($user_id, "s2member_custom_fields", $fields);
+							}
+					}
+				/**/
+				setcookie ("s2member_subscr_id", "", time () + 31556926, "/");
+				setcookie ("s2member_custom", "", time () + 31556926, "/");
+				setcookie ("s2member_level", "", time () + 31556926, "/");
+			}
+		/**/
+		return;
+	}
+?>

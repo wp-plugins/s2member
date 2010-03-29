7b1f1709eeaed97e590bea3b0566cbb21eb5d6e8
PriMoThemes <PriMoThemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-29 03:26:12 +0000

    git-svn-id: https://plugins.svn.wordpress.org/s2member/trunk@222836 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/menu-pages/con-samps/current_user_subscr_id.php
+++
@@ -1,3 +0,0 @@
-<?php echo S2MEMBER_CURRENT_USER_SUBSCR_ID; ?>
-This may output something like: S-82234JD0923423
-( this is the PayPal Subscription ID associated with their account )
--- includes/menu-pages/con-samps/level1_label.php
+++
@@ -1,3 +0,0 @@
-<?php echo S2MEMBER_LEVEL1_LABEL; ?>
-This may output something like: Bronze Membership
-( or whatever label you've configured for level #1 )

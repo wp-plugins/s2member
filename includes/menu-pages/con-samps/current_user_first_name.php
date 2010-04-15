9031f3cf64c7021fb4c09711a9f47bafd55865b4
PriMoThemes <PriMoThemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-15 01:29:19 +0000

    git-svn-id: https://plugins.svn.wordpress.org/s2member/trunk@229414 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/menu-pages/con-samps/current_user_first_name.php
+++
@@ -1,3 +0,0 @@
-<?php echo S2MEMBER_CURRENT_USER_FIRST_NAME; ?>
-This may output something like: John
-( or whatever their first name is )
--- includes/menu-pages/con-samps/level2_label.php
+++
@@ -1,3 +0,0 @@
-<?php echo S2MEMBER_LEVEL2_LABEL; ?>
-This may output something like: Silver Membership
-( or whatever label you've configured for level #2 )

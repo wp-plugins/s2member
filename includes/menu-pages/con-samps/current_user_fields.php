a13d7a7354f48f38c82196b325f371ba151f9048
PriMoThemes <PriMoThemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-17 04:03:49 +0000

    git-svn-id: https://plugins.svn.wordpress.org/s2member/tags/2.8.4@230141 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/menu-pages/con-samps/current_user_fields.php
+++
@@ -1,7 +0,0 @@
-<?php $fields = unserialize(S2MEMBER_CURRENT_USER_FIELDS); ?>
-<?php echo $fields["first_name"]; ?> <?php echo $fields["last_name"]; ?>
-This would output the first and last name for the current user.
-
-Custom Fields are also included in the unserialized array.
-<?php print_r(unserialize(S2MEMBER_CURRENT_USER_FIELDS)); ?>
-( Displays a full list of all array elements. )

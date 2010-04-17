16b775c51e64ced6467491a97b0db01dfa2ced39
PriMoThemes <PriMoThemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-17 04:02:45 +0000

    git-svn-id: https://plugins.svn.wordpress.org/s2member/trunk@230140 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/menu-pages/drip-samps.php
+++
@@ -1,9 +0,0 @@
-<?php if(S2MEMBER_CURRENT_USER_REGISTRATION_DAYS >= 90){ ?>
-	Drip content to Members who are more than 90 days old.
-<?php } else if(S2MEMBER_CURRENT_USER_REGISTRATION_DAYS >= 60){ ?>
-	Drip content to Members who are more than 60 days old.
-<?php } else if(S2MEMBER_CURRENT_USER_REGISTRATION_DAYS >= 30){ ?>
-	Drip content to Members who are more than 30 days old.
-<?php } else { ?>
-	Initial content for newer Members.
-<?php } ?>

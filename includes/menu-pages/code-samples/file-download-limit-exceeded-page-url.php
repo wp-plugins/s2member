cddd528080c24cd94e9a4bd5f1fb6f546b6f61d3
PriMoThemes <PriMoThemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-01 11:42:28 +0000

    git-svn-id: https://plugins.svn.wordpress.org/s2member/tags/2.8.8@236162 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/menu-pages/code-samples/file-download-limit-exceeded-page-url.php
+++
@@ -1,3 +0,0 @@
-<?php if (S2MEMBER_CURRENT_USER_DOWNLOADS_CURRENTLY > S2MEMBER_CURRENT_USER_DOWNLOADS_ALLOWED){ ?>
-  Limit Exceeded, <a href="<?php echo S2MEMBER_FILE_DOWNLOAD_LIMIT_EXCEEDED_PAGE_URL; ?>">click here for details</a>.
-<?php } ?>

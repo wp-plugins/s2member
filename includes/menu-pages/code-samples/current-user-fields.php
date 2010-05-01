590ef583063c08ffd160a49948a70d14d80297f8
PriMoThemes <PriMoThemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-01 12:20:12 +0000

    git-svn-id: https://plugins.svn.wordpress.org/s2member/tags/2.8.8@236172 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/menu-pages/code-samples/current-user-fields.php
+++
@@ -1,7 +0,0 @@
-<?php $fields = json_decode(S2MEMBER_CURRENT_USER_FIELDS, true); ?>
-<?php echo $fields["first_name"]; ?> <?php echo $fields["last_name"]; ?>
-This would output the first and last name for the current user.
-
-Custom Fields are also included in the JSON decoded array.
-<?php print_r(json_decode(S2MEMBER_CURRENT_USER_FIELDS, true)); ?>
-( Displays a full list of all associative array elements. )

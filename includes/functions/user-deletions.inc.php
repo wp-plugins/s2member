b25170dcf09305d3251432d5580970928d7941a0
PriMoThemes <PriMoThemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-23 13:09:14 +0000

    git-svn-id: https://plugins.svn.wordpress.org/s2member/trunk@244746 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/functions/user-deletions.inc.php
@@ -0,0 +1,47 @@
+<?php
+/*
+Copyright: © 2009 WebSharks, Inc. ( coded in the USA )
+<mailto:support@websharks-inc.com> <http://www.websharks-inc.com/>
+
+Released under the terms of the GNU General Public License.
+You should have received a copy of the GNU General Public License,
+along with this software. In the main directory, see: /licensing/
+If not, see: <http://www.gnu.org/licenses/>.
+*/
+/*
+Direct access denial.
+*/
+if (realpath (__FILE__) === realpath ($_SERVER["SCRIPT_FILENAME"]))
+	exit;
+/*
+Function for handling user deletions.
+Attach to: add_action("delete_user");
+*/
+function ws_plugin__s2member_handle_user_deletions ($user_id = FALSE)
+	{
+		do_action ("s2member_before_handle_user_deletions");
+		/**/
+		$user = new WP_User ($user_id); /* Acquire user obj. */
+		/**/
+		$custom = get_usermeta ($user_id, "s2member_custom");
+		$subscr_id = get_usermeta ($user_id, "s2member_subscr_id");
+		/**/
+		if (is_object ($user) && $subscr_id && $custom && $GLOBALS["WS_PLUGIN__"]["s2member"]["o"]["eot_del_notification_urls"] && is_array ($cv = preg_split ("/\|/", $custom)))
+			{
+				foreach (preg_split ("/[\r\n\t]+/", $GLOBALS["WS_PLUGIN__"]["s2member"]["o"]["eot_del_notification_urls"]) as $url) /* Handle eot notifications on user deletion. */
+					if (($url = preg_replace ("/%%cv([0-9]+)%%/ei", 'urlencode(trim($cv[$1]))', $url)) && ($url = preg_replace ("/%%subscr_id%%/i", urlencode ($subscr_id), $url)))
+						if (($url = preg_replace ("/%%user_first_name%%/i", urlencode ($user->first_name), $url)) && ($url = preg_replace ("/%%user_last_name%%/i", urlencode ($user->last_name), $url)))
+							if (($url = preg_replace ("/%%user_full_name%%/i", urlencode (trim ($user->first_name . " " . $user->last_name)), $url)))
+								if (($url = preg_replace ("/%%user_email%%/i", urlencode ($user->user_email), $url)))
+									/**/
+									if (($url = trim ($url))) /* Empty? */
+										ws_plugin__s2member_remote ($url);
+				/**/
+				do_action ("s2member_during_handle_user_deletions");
+			}
+		/**/
+		do_action ("s2member_after_handle_user_deletions");
+		/**/
+		return;
+	}
+?>
---
+++ includes/menu-pages/code-samples/current-user-custom.php
@@ -0,0 +1,3 @@
+<?php echo S2MEMBER_CURRENT_USER_CUSTOM; ?>
+This may output something like: www.yourdomain.com|cv1|cv2|cv3
+( this is the PayPal input field value for the `custom` variable )

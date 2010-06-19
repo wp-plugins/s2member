0fbe09330c5918aa6c167223f5b675911bb53632
PriMoThemes <PriMoThemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-19 19:27:06 +0000

    git-svn-id: https://plugins.svn.wordpress.org/s2member/tags/3.0.6@254829 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/menu-pages/code-samples/level0-label.php
@@ -0,0 +1,3 @@
+<?php echo S2MEMBER_LEVEL0_LABEL; ?>
+This may output something like: Free
+( or whatever Label you've configured for Level #0 )
---
+++ includes/menu-pages/code-samples/level2-file-downloads-allowed-days.php
@@ -0,0 +1,3 @@
+Level #2 Members are allowed to download
+<?php echo S2MEMBER_LEVEL2_FILE_DOWNLOADS_ALLOWED; ?> files
+every <?php S2MEMBER_LEVEL2_FILE_DOWNLOADS_ALLOWED_DAYS; ?> days.

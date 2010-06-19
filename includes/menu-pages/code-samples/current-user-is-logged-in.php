40bd3e0229619ccbb58ff0d846746cea77fa2d95
PriMoThemes <PriMoThemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-19 19:21:59 +0000

    git-svn-id: https://plugins.svn.wordpress.org/s2member/trunk@254826 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/menu-pages/code-samples/current-user-is-logged-in.php
+++
@@ -1,3 +0,0 @@
-<?php if (S2MEMBER_CURRENT_USER_IS_LOGGED_IN){ ?>
-  A User/Member is logged in, with an Access Level >= 0.
-<?php } ?>
--- includes/menu-pages/code-samples/level1-label.php
+++
@@ -1,3 +0,0 @@
-<?php echo S2MEMBER_LEVEL1_LABEL; ?>
-This may output something like: Bronze Membership
-( or whatever Label you've configured for Level #1 )

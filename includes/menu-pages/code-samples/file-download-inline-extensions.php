d3fcf41c345a823862a9e063556208c79eacfc8d
PriMoThemes <PriMoThemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-27 06:25:40 +0000

    git-svn-id: https://plugins.svn.wordpress.org/s2member/trunk@257704 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/menu-pages/code-samples/file-download-inline-extensions.php
+++
@@ -1,3 +0,0 @@
-<?php echo S2MEMBER_FILE_DOWNLOAD_INLINE_EXTENSIONS; ?>
-This may output something like: pdf,jpg,jpeg,jpe,png
-( s2Member will display these extensions inline )
--- includes/menu-pages/code-samples/level0-label.php
+++
@@ -1,3 +0,0 @@
-<?php echo S2MEMBER_LEVEL0_LABEL; ?>
-This may output something like: Free
-( or whatever Label you've configured for Level #0 )

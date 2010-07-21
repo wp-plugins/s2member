ee38b9974d8632c8d730a433b41f781b3f55f607
PriMoThemes <PriMoThemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-21 13:27:11 +0000

    git-svn-id: https://plugins.svn.wordpress.org/s2member/trunk@265191 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/menu-pages/code-samples/current-user-is-logged-in-as-member.php
+++
@@ -1,3 +0,0 @@
-<?php if (S2MEMBER_CURRENT_USER_IS_LOGGED_IN_AS_MEMBER){ ?>
-  A Member is logged in, with an Access Level >= 1.
-<?php } ?>
--- includes/menu-pages/code-samples/level4-label.php
+++
@@ -1,3 +0,0 @@
-<?php echo S2MEMBER_LEVEL4_LABEL; ?>
-This may output something like: Platinum Membership
-( or whatever Label you've configured for Level #4 )

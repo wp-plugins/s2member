10d0d0f050775f6cd1996a64ebbde427750ecef2
PriMoThemes <PriMoThemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-21 13:32:15 +0000

    git-svn-id: https://plugins.svn.wordpress.org/s2member/tags/3.1.3@265193 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/menu-pages/code-samples/level0-file-downloads-allowed.php
@@ -0,0 +1,3 @@
+Level #0 Users are allowed to download
+<?php echo S2MEMBER_LEVEL0_FILE_DOWNLOADS_ALLOWED; ?> files
+every <?php S2MEMBER_LEVEL0_FILE_DOWNLOADS_ALLOWED_DAYS; ?> days.
---
+++ includes/menu-pages/code-samples/level2-label.php
@@ -0,0 +1,3 @@
+<?php echo S2MEMBER_LEVEL2_LABEL; ?>
+This may output something like: Silver Membership
+( or whatever Label you've configured for Level #2 )

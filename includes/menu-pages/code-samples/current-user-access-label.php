6606b7889cef91bd30523366b89fc6aec9e5b3e5
PriMoThemes <PriMoThemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-22 06:43:56 +0000

    git-svn-id: https://plugins.svn.wordpress.org/s2member/trunk@265731 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/menu-pages/code-samples/current-user-access-label.php
+++
@@ -1,3 +0,0 @@
-<?php echo S2MEMBER_CURRENT_USER_ACCESS_LABEL; ?>
-This may output something like: Gold Membership
-( or whatever Label you've configured for their Membership Level )
--- includes/menu-pages/code-samples/current-user-login.php
+++
@@ -1,3 +0,0 @@
-<?php echo S2MEMBER_CURRENT_USER_LOGIN; ?>
-This may output something like: johnsmith22
-( or whatever their login/username is )

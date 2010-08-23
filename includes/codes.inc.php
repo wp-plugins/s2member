c9f00967851ef6df6df8d6fd92fd94b4a55acbb0
PriMoThemes <PriMoThemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-23 12:24:16 +0000

    git-svn-id: https://plugins.svn.wordpress.org/s2member/tags/3.2.2@279623 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/codes.inc.php
@@ -0,0 +1,45 @@
+<?php
+/*
+Copyright: © 2009 WebSharks, Inc. ( coded in the USA )
+<mailto:support@websharks-inc.com> <http://www.websharks-inc.com/>
+
+Released under the terms of the GNU General Public License.
+You should have received a copy of the GNU General Public License,
+along with this software. In the main directory, see: /licensing/
+If not, see: <http://www.gnu.org/licenses/>.
+*/
+/*
+Direct access denial.
+*/
+if (realpath (__FILE__) === realpath ($_SERVER["SCRIPT_FILENAME"]))
+	exit ("Do not access this file directly.");
+/*
+Add WordPress® Editor Shortcodes.
+http://codex.wordpress.org/Shortcode_API
+*/
+add_shortcode ("s2Get", "ws_plugin__s2member_sc_get_details");
+/**/
+add_shortcode ("s2If", "ws_plugin__s2member_sc_if_conditionals");
+add_shortcode ("_s2If", "ws_plugin__s2member_sc_if_conditionals");
+add_shortcode ("__s2If", "ws_plugin__s2member_sc_if_conditionals");
+add_shortcode ("___s2If", "ws_plugin__s2member_sc_if_conditionals");
+/**/
+add_shortcode ("s2Member-PayPal-Button", "ws_plugin__s2member_sc_paypal_button");
+/*
+----------------------------------------------------------------------------------------
+The following Shortcodes were deprecated in v3.2.2.
+Going forward, use [s2If] instead ( it's more powerful ).
+@NOTE: These Shortcodes were ONLY available in v3.2.1.
+~ These WILL all be removed in a future release.
+----------------------------------------------------------------------------------------
+*/
+add_shortcode ("s2All", "ws_plugin__s2member_sc_old_conditionals");
+add_shortcode ("_s2All", "ws_plugin__s2member_sc_old_conditionals");
+add_shortcode ("__s2All", "ws_plugin__s2member_sc_old_conditionals");
+add_shortcode ("___s2All", "ws_plugin__s2member_sc_old_conditionals");
+/**/
+add_shortcode ("s2Any", "ws_plugin__s2member_sc_old_conditionals");
+add_shortcode ("_s2Any", "ws_plugin__s2member_sc_old_conditionals");
+add_shortcode ("__s2Any", "ws_plugin__s2member_sc_old_conditionals");
+add_shortcode ("___s2Any", "ws_plugin__s2member_sc_old_conditionals");
+?>

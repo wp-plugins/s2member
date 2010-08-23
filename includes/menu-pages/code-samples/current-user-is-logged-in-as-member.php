f37849c3c1475eeb70894aded6a24a3dcbed1522
PriMoThemes <PriMoThemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-23 12:16:18 +0000

    git-svn-id: https://plugins.svn.wordpress.org/s2member/trunk@279618 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/menu-pages/code-samples/current-user-is-logged-in-as-member.php
+++
@@ -1,9 +0,0 @@
-<?php if (S2MEMBER_CURRENT_USER_IS_LOGGED_IN_AS_MEMBER){ ?>
-  A Member is logged in, with an Access Level >= 1.
-<?php } ?>
-
----- s2member Shortcode Equivalent ----
-
-[s2All is_user_logged_in="yes" current_user_can="access_s2member_level1"]
-	content goes here
-[/s2All]

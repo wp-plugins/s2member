6bbe1652d965758809396eef57d9fce25166b1e5
PriMoThemes <PriMoThemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-30 02:14:42 +0000

    git-svn-id: https://plugins.svn.wordpress.org/s2member/trunk@282423 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/menu-pages/code-samples/current-user-profile-modification-page-url-1.php
@@ -0,0 +1,5 @@
+<a href="<?php echo S2MEMBER_CURRENT_USER_PROFILE_MODIFICATION_PAGE_URL; ?>">Modify Profile</a>
+
+---- s2member Shortcode Equivalent ----
+
+[s2Get constant="S2MEMBER_CURRENT_USER_PROFILE_MODIFICATION_PAGE_URL" /]

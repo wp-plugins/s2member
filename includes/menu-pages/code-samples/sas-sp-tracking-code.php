ab7365a1e129eeec3fbe42d035c0c5df0747fe42
PriMoThemes <PriMoThemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-30 01:01:25 +0000

    git-svn-id: https://plugins.svn.wordpress.org/s2member/trunk@282409 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/menu-pages/code-samples/sas-sp-tracking-code.php
+++
@@ -1,6 +0,0 @@
-amount=%%amount%%
-tracking=%%txn_id%%
-
-<img src="https://shareasale.com/sale.cfm
-?amount=%%amount%%&tracking=%%txn_id%%&transtype=SALE&merchantID=123"
- width="1" height="1" border="0" />

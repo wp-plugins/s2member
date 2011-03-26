8edab6a21fcc124eddc793195cd46f79104b1aff
PriMoThemes <PriMoThemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-26 03:39:13 +0000

    git-svn-id: https://plugins.svn.wordpress.org/s2member/trunk@364887 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/menu-pages/code-samples/current-user-profile-modification-page-url-1.php
+++
@@ -1,5 +0,0 @@
-<a href="<?php echo S2MEMBER_CURRENT_USER_PROFILE_MODIFICATION_PAGE_URL; ?>">Modify Profile</a>
-
----- s2member Shortcode Equivalent ----
-
-<a href="[s2Get constant="S2MEMBER_CURRENT_USER_PROFILE_MODIFICATION_PAGE_URL" /]">Modify Profile</a>
--- includes/menu-pages/code-samples/mms-patch-wp-login.php
+++
@@ -1,7 +0,0 @@
-	case 'register' :
-	if ( is_multisite() ) {
-		// Multisite uses wp-signup.php
-		// Modified for full plugin compatiblity.
-		//wp_redirect( apply_filters( 'wp_signup_location', get_bloginfo('wpurl') . '/wp-signup.php' ) );
-		//exit;
-	}
